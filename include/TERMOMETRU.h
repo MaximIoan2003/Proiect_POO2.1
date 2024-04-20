bc930bd446a39b54132f90997dcd0f9df6b94741
ioanMAX2003 <164477293+ioanMAX2003@users.noreply.github.com>
2024-04-20 19:37:11 +0300

    Create TERMOMETRU.h

---
+++ include/TERMOMETRU.h
@@ -0,0 +1,30 @@
+#ifndef TERMOMETRU_H
+#define TERMOMETRU_H
+
+#include <string>
+
+class Termometru {
+private:
+    std::string firma;
+    int pret;
+public:
+    Termometru(const std::string firma1, int pret1);
+
+    Termometru(const Termometru& T);
+
+    Termometru& operator=(const Termometru& T);
+
+    virtual ~Termometru();
+
+    virtual void temp_max() const = 0;
+
+    virtual void baterie() const = 0;
+
+    virtual Termometru* copie() const = 0;
+
+    std::string getFirma() const;
+
+    int getPret() const;
+};
+
+#endif
