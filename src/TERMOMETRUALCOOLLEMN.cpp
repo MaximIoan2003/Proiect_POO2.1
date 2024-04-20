d1ed86300398b8efcad8a0ad6590cb915a9be790
ioanMAX2003 <164477293+ioanMAX2003@users.noreply.github.com>
2024-04-20 19:44:23 +0300

    Create TERMOMETRUALCOOLLEMN.cpp

---
+++ src/TERMOMETRUALCOOLLEMN.cpp
@@ -0,0 +1,26 @@
+#include "Termometru_alcool_lemn.h"
+
+Termometru_alcool_lemn::Termometru_alcool_lemn(const std::string firma, int pret) : Termometru(firma, pret) {}
+
+Termometru_alcool_lemn::Termometru_alcool_lemn(const Termometru_alcool_lemn& T) : Termometru(T) {}
+
+Termometru_alcool_lemn& Termometru_alcool_lemn::operator=(const Termometru_alcool_lemn& T) {
+    if(this != &T) {
+        Termometru::operator=(T);
+    }
+    return *this;
+}
+
+Termometru_alcool_lemn::~Termometru_alcool_lemn() {}
+
+void Termometru_alcool_lemn::temp_max() const {
+    std::cout << "Temperatura maxima a termometrului cu alcool din lemn " << getFirma() << " este de 50 de grade";
+}
+
+void Termometru_alcool_lemn::baterie() const {
+    std::cout << "Termometrul " << getFirma()  << " nu are baterie !";
+}
+
+Termometru* Termometru_alcool_lemn::copie() const {
+    return new Termometru_alcool_lemn(*this);
+}
