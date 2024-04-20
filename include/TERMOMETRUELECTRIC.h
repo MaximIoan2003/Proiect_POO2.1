cd6431382a9fdcc042a3c08f079276253826b572
ioanMAX2003 <164477293+ioanMAX2003@users.noreply.github.com>
2024-04-20 19:46:34 +0300

    Create TERMOMETRUELECTRIC.h

---
+++ include/TERMOMETRUELECTRIC.h
@@ -0,0 +1,28 @@
+#ifndef TERMOMETRUELECTRIC_H
+#define TERMOMETRUELECTRIC_H
+
+#include "Termometru.h"
+#include <iostream>
+
+class Termometru_electric : public Termometru {
+private:
+    int capacitate;
+public:
+    Termometru_electric(const std::string firma, int pret, int capacitate);
+
+    Termometru_electric(const Termometru_electric& T);
+
+    Termometru_electric& operator=(const Termometru_electric& T);
+
+    ~Termometru_electric();
+
+    int cap() const;
+
+    void temp_max() const override;
+
+    void baterie() const override;
+
+    Termometru* copie() const override;
+};
+
+#endif
