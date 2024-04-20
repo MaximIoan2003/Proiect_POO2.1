97e8e6f86ffd6c73a5cf88c1856611cfd848fe9f
ioanMAX2003 <164477293+ioanMAX2003@users.noreply.github.com>
2024-04-20 21:51:05 +0300

    Update TERMOMETRUELECTRIC.h

--- include/TERMOMETRUELECTRIC.h
+++ include/TERMOMETRUELECTRIC.h
@@ -5,18 +5,14 @@
 #include <iostream>
 
 class Termometru_electric : public Termometru {
-private:
-    int capacitate;
 public:
-    Termometru_electric(const std::string& firma, int pret, int capacitate);
+    Termometru_electric(const std::string& firma, int pret);
 
     Termometru_electric(const Termometru_electric& T);
 
     Termometru_electric& operator=(const Termometru_electric& T);
 
     ~Termometru_electric();
-
-    int cap() const;
 
     void temp_max() const override;
 
