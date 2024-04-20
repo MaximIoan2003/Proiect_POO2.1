a8e8c73b26a975de19b8a74e38e04e476e29e07d
ioanMAX2003 <164477293+ioanMAX2003@users.noreply.github.com>
2024-04-20 19:48:12 +0300

    Create MARFA.cpp

---
+++ src/MARFA.cpp
@@ -0,0 +1,27 @@
+#include "Marfa.h"
+
+Marfa::Marfa() : medicamente(""), pret_actual(0) {}
+
+Marfa::Marfa(const std::string& m, int pret) : medicamente(m), pret_actual(pret) {}
+
+std::string Marfa::medicine() const {
+    return medicamente;
+}
+
+int Marfa::price() const {
+    return pret_actual;
+}
+
+Marfa operator+(const Marfa& obj1, const Marfa& obj2) {
+    std::string nouN = obj1.medicine() + "-" + obj2.medicine();
+    int nouP = obj1.price() + obj2.price();
+    return Marfa(nouN, nouP);
+}
+
+Marfa operator/(const Marfa& obj3, int nr) {
+    if (nr != 0) {
+        return Marfa(obj3.medicine(), obj3.price() / nr);
+    } else {
+        return Marfa();
+    }
+}
