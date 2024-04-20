1b4c25c370e5d073e8c9bdf9a8578cd41d74f7ee
ioanMAX2003 <164477293+ioanMAX2003@users.noreply.github.com>
2024-04-20 21:52:21 +0300

    Update TERMOMETRUELECTRIC.cpp

--- src/TERMOMETRUELECTRIC.cpp
+++ src/TERMOMETRUELECTRIC.cpp
@@ -1,6 +1,6 @@
 #include "TERMOMETRUELECTRIC.h"
 
-Termometru_electric::Termometru_electric(const std::string& firma, int pret, int capacitate) : Termometru(firma, pret), capacitate(capacitate) {}
+Termometru_electric::Termometru_electric(const std::string& firma, int pret) : Termometru(firma, pret) {}
 
 Termometru_electric::Termometru_electric(const Termometru_electric& T) : Termometru(T) {}
 
@@ -13,18 +13,13 @@ Termometru_electric& Termometru_electric::operator=(const Termometru_electric& T
 
 Termometru_electric::~Termometru_electric() {}
 
-int Termometru_electric::cap() const {
-    return capacitate;
-}
 
 void Termometru_electric::temp_max() const {
     std::cout << "Temperatura maxima a termometrului electric" << getFirma() << " este de 100 de grade";
 }
 
 void Termometru_electric::baterie() const {
-    std::cout << "Termometrul " << getFirma() << " are o capacitate la baterie de : " << cap() << " si costa : " << getPret();
+    std::cout << "Termometrul costa : " << getPret();
 }
 
-Termometru* Termometru_electric::copie() const {
-    return new Termometru_electric(*this);
-}
+
